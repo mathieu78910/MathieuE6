f72f142f3d3875a0d3ce49d160a8ba8219328f0c
Mathieu7 <mathieu.monnie@campus-la-chataigneraie.org>
2025-03-24 00:05:41 +0100

    changement

--- src/components/StagesSection.tsx
+++ src/components/StagesSection.tsx
@@ -67,7 +67,7 @@ const StagesSection = ({ stages }: StagesSectionProps) => {
                         className="button-style flex items-center px-4 py-2"
                       >
                         <Download className="w-5 h-5 mr-2" />
-                        Rapport
+                        Preuve
                       </a>
                     )}
                     {stage.attestationUrl && (
